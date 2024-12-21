0c70de3ac8cdf00ce81bdefa992d40e0d0a37be5
dishowrov <dinislamshowrov2001@gmail.com>
2024-12-21 11:18:48 +0600

    :new:

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,12 @@ const menuList = document.querySelector(".header-menu");
 const openMenu = document.querySelector("button.open-menu");
 const closeMenu = document.querySelector("button.close-menu");
 
+const creditBtn = document.getElementById("passbook-btn-credit");
+const debitBtn = document.getElementById("passbook-btn-debit");
+const creditList = document.getElementById("passbook-list-credit");
+const debitList = document.getElementById("passbook-list-debit");
+
+
 
 openMenu.addEventListener("click", () => {
     menuList.style.display = "block";
@@ -14,4 +20,19 @@ closeMenu.addEventListener("click", () => {
     menuList.style.display = "none";
     closeMenu.style.display = "none";
     openMenu.style.display = "block";
+});
+
+
+creditBtn.addEventListener("click", () => {
+    creditList.style.display = "block";
+    debitList.style.display = "none";
+    creditBtn.classList.add("active");
+    debitBtn.classList.remove("active");
+});
+
+debitBtn.addEventListener("click", () => {
+    debitList.style.display = "block";
+    creditList.style.display = "none";
+    debitBtn.classList.add("active");
+    creditBtn.classList.remove("active");
 });
